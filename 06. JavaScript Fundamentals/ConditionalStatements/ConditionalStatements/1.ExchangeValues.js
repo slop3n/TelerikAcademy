86f54489666b958c5c2252e9e46ef3c60490736e
slop3n <slop3n@gmail.com>
2014-05-06 21:41:16 +0300

    more homeworks

---
+++ 06. JavaScript Fundamentals/ConditionalStatements/ConditionalStatements/1.ExchangeValues.js
@@ -0,0 +1,12 @@
+﻿function exchangeValues() {
+    var firstNum = jsConsole.readInteger("#inputOne");
+        secondNum = jsConsole.readInteger("#inputTwo");
+
+    if (firstNum > secondNum) {
+        var temp = secondNum;
+        secondNum = firstNum;
+        firstNum = temp;
+    }
+
+    jsConsole.writeLine("First number is: " + firstNum + " Second number is: " + secondNum);
+}
---
+++ 06. JavaScript Fundamentals/ConditionalStatements/ConditionalStatements/2.SignOfProduct.js
@@ -0,0 +1,20 @@
+﻿function getSign() {
+    var numOne = jsConsole.readInteger("#inputOne");
+    numTwo = jsConsole.readInteger("#inputTwo");
+    numThree = jsConsole.readInteger("#inputThree");
+    if (numOne < 0 && (numTwo < 0 || numThree < 0)) {
+        jsConsole.writeLine("The sign is positive")
+    }
+    else if (numTwo < 0 && (numOne < 0 || numThree < 0)) {
+        jsConsole.writeLine("The sign is positive")
+    }
+    else if (numThree < 0 && (numOne < 0 || numTwo < 0)) {
+        jsConsole.writeLine("The sign is positive")
+    }
+    else if (numOne > 0 && numTwo > 0 && numThree > 0) {
+        jsConsole.writeLine("The sign is positive")
+    }
+    else {
+        jsConsole.writeLine("The sign is negative")
+    }
+}
