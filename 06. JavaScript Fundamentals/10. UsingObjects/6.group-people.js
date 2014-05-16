412396abe469c98fedfff4c1243d858f27c0bae4
slop3n <slop3n@gmail.com>
2014-05-16 12:40:16 +0300

    Finished Objects homework

--- 06. JavaScript Fundamentals/10. UsingObjects/6.group-people.js
+++ 06. JavaScript Fundamentals/10. UsingObjects/6.group-people.js
@@ -3,19 +3,33 @@ age, first or last name. The function must return an
 associative array, with keys - the groups, and values -
 arrays with persons in this groups
  Use function overloading (i.e. just one function) */
-function dynamicSort(property) {
-    var sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result * sortOrder;
+ function group(people, prop) {
+    switch (prop) {
+        case "firstName":
+        case "lastName":
+        case "age":
+            {
+                var groups = {};
+
+                people.map(function (p) {
+                    if (!groups[p[prop]]){
+                        groups[p[prop]] = new Array();
+                    }
+                    groups[p[prop]].push(p);
+                });
+
+                return groups;
+            }
+        default:
+            throw new Error("No such property in Person.");
     }
 }
+
 
 var People = [
 { firstname:'Gosho', lastname: 'Petrov', age: 32 },
 { firstname:'Bay', lastname: 'Ivan', age: 82 },
 { firstname:'Naughty', lastname: 'Joro', age: 20 } ];
+
+var grouped = group(People, "age");
+console.log(grouped)
